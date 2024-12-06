5cea8900bff29a19e829157e7cc7e992dc759fe5
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2024-12-05 17:36:05 -0800

    Update utils.cu
    no change for scale.
    Need to delete the input scale in the future

--- submodules/diff-gaussian-rasterization/cuda_rasterizer/utils.cu
+++ submodules/diff-gaussian-rasterization/cuda_rasterizer/utils.cu
@@ -23,16 +23,16 @@ __global__ void compute_relocation(
     opacity_new[idx] = 1.0f - powf(1.0f - opacity_old[idx], 1.0f / N_idx);
     
     // compute new scale
-    for (int i = 1; i <= N_idx; ++i) {
-        for (int k = 0; k <= (i-1); ++k) {
-            float bin_coeff = binoms[(i-1) * n_max + k];
-            float term = (powf(-1, k) / sqrtf(k + 1)) * powf(opacity_new[idx], k + 1);
-            denom_sum += (bin_coeff * term);
-        }
-    }
-    float coeff = (opacity_old[idx] / denom_sum);
+    // for (int i = 1; i <= N_idx; ++i) {
+    //     for (int k = 0; k <= (i-1); ++k) {
+    //         float bin_coeff = binoms[(i-1) * n_max + k];
+    //         float term = (powf(-1, k) / sqrtf(k + 1)) * powf(opacity_new[idx], k + 1);
+    //         denom_sum += (bin_coeff * term);
+    //     }
+    // }
+    // float coeff = (opacity_old[idx] / denom_sum);
     for (int i = 0; i < 3; ++i)
-        scale_new[idx * 3 + i] = coeff * scale_old[idx * 3 + i];
+        scale_new[idx * 3 + i] = scale_old[idx * 3 + i];
 }
 
 void UTILS::ComputeRelocation(
