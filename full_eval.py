fbff5c5497f312c05a53dda4114837065d6a1640
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2024-12-05 17:45:16 -0800

    Update full_eval.py

--- full_eval.py
+++ full_eval.py
@@ -12,10 +12,21 @@
 import os
 from argparse import ArgumentParser
 
+cap_max = {
+    "bicycle": 6_000_000, 
+    "flowers": 3_000_000,
+    "garden": 5_000_000, 
+    "stump": 4_500_000, 
+    "treehill": 3_500_000,
+    "room": 1_500_000, 
+    "counter": 1_500_000,
+    "kitchen": 1_500_000, 
+    "bonsai": 1_500_000
+}
 mipnerf360_outdoor_scenes = ["bicycle", "flowers", "garden", "stump", "treehill"]
 mipnerf360_indoor_scenes = ["room", "counter", "kitchen", "bonsai"]
-tanks_and_temples_scenes = ["truck", "train"]
-deep_blending_scenes = ["drjohnson", "playroom"]
+# tanks_and_temples_scenes = ["truck", "train"]
+# deep_blending_scenes = ["drjohnson", "playroom"]
 
 parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument("--skip_training", action="store_true")
@@ -27,29 +38,29 @@ args, _ = parser.parse_known_args()
 all_scenes = []
 all_scenes.extend(mipnerf360_outdoor_scenes)
 all_scenes.extend(mipnerf360_indoor_scenes)
-all_scenes.extend(tanks_and_temples_scenes)
-all_scenes.extend(deep_blending_scenes)
+# all_scenes.extend(tanks_and_temples_scenes)
+# all_scenes.extend(deep_blending_scenes)
 
 if not args.skip_training or not args.skip_rendering:
     parser.add_argument('--mipnerf360', "-m360", required=True, type=str)
-    parser.add_argument("--tanksandtemples", "-tat", required=True, type=str)
-    parser.add_argument("--deepblending", "-db", required=True, type=str)
+    # parser.add_argument("--tanksandtemples", "-tat", required=True, type=str)
+    # parser.add_argument("--deepblending", "-db", required=True, type=str)
     args = parser.parse_args()
 
 if not args.skip_training:
-    common_args = " --quiet --eval --test_iterations -1 "
+    common_args = " --quiet --eval --test_iterations -1 --init_type sfm "
     for scene in mipnerf360_outdoor_scenes:
         source = args.mipnerf360 + "/" + scene
-        os.system("python train.py -s " + source + " -i images_4 -m " + args.output_path + "/" + scene + common_args)
+        os.system("python train.py -s " + source + " -i images_4 -m " + args.output_path + "/" + scene + f" --cap_max {cap_max[scene]}" + common_args)
     for scene in mipnerf360_indoor_scenes:
         source = args.mipnerf360 + "/" + scene
-        os.system("python train.py -s " + source + " -i images_2 -m " + args.output_path + "/" + scene + common_args)
-    for scene in tanks_and_temples_scenes:
-        source = args.tanksandtemples + "/" + scene
-        os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args)
-    for scene in deep_blending_scenes:
-        source = args.deepblending + "/" + scene
-        os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args)
+        os.system("python train.py -s " + source + " -i images_2 -m " + args.output_path + "/" + scene + f" --cap_max {cap_max[scene]}" + common_args)
+    # for scene in tanks_and_temples_scenes:
+    #     source = args.tanksandtemples + "/" + scene
+    #     os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args)
+    # for scene in deep_blending_scenes:
+    #     source = args.deepblending + "/" + scene
+    #     os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args)
 
 if not args.skip_rendering:
     all_sources = []
@@ -57,10 +68,10 @@ if not args.skip_rendering:
         all_sources.append(args.mipnerf360 + "/" + scene)
     for scene in mipnerf360_indoor_scenes:
         all_sources.append(args.mipnerf360 + "/" + scene)
-    for scene in tanks_and_temples_scenes:
-        all_sources.append(args.tanksandtemples + "/" + scene)
-    for scene in deep_blending_scenes:
-        all_sources.append(args.deepblending + "/" + scene)
+    # for scene in tanks_and_temples_scenes:
+    #     all_sources.append(args.tanksandtemples + "/" + scene)
+    # for scene in deep_blending_scenes:
+    #     all_sources.append(args.deepblending + "/" + scene)
 
     common_args = " --quiet --eval --skip_train"
     for scene, source in zip(all_scenes, all_sources):
