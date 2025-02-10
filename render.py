b88b06a633f75989ad2722a599e01c9634489083
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2025-02-10 12:38:18 -0800

    Update render.py

--- render.py
+++ render.py
@@ -19,14 +19,15 @@ from PIL import Image
 from utils.camera_utils import generate_interpolated_path, generate_ellipse_path_z
 import numpy as np
 import imageio
+from tqdm import tqdm
 
 
-@torch.no_grad
+@torch.no_grad()
 def rendering(args):
+    args.data_device = "cpu"
     gaussians = GaussianModel(args.sh_degree, args.sb_number)
     scene = Scene(
-        args, gaussians, args.loading_iteration, shuffle=False, center_and_z_up=True
-    )
+        args, gaussians, args.loading_iteration, shuffle=False)
 
     bg_color = [1, 1, 1] if args.white_background else [0, 0, 0]
     gaussians.background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
@@ -49,7 +50,7 @@ def rendering(args):
             im.save(os.path.join(output_dir, f"{render_mode}_{i}.png"))
             print(f"Saved {render_mode}_{i}.png")
 
-    def render_video(cameras, render_mode, render_traj_path_type="ellipse", fps=30):
+    def render_video(cameras, render_mode, render_traj_path_type, fps=30):
         if render_traj_path_type == "ellipse":
             trajectory_cameras = generate_ellipse_path_z(cameras)
         elif render_traj_path_type == "interp":
@@ -62,25 +63,24 @@ def rendering(args):
         writer = imageio.get_writer(video_path, fps=fps)
 
         # Render each frame and write directly to the video
-        for i, cam in enumerate(trajectory_cameras):
+        for i, cam in tqdm(enumerate(trajectory_cameras)):
             render_pkg = gaussians.render(cam, render_mode=render_mode)
             image = render_pkg["render"]
             # Convert image to uint8 format
             image_cpu = image.clamp(0, 1).cpu().permute(1, 2, 0).numpy()  # [H, W, C]
             frame = (image_cpu * 255).astype(np.uint8)
             writer.append_data(frame)
+
         writer.close()
         print(f"Video saved to {video_path}")
 
-    if args.image:
-        print("Rendering images...")
-        render_image(cameras, args.render_mode)
+    print("Rendering images...")
+    render_image(cameras, args.render_mode)
 
     print("Rendering videos...")
     render_video(cameras, args.render_mode, args.render_traj_path)
 
     print("Rendering complete.")
-
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Rendering script parameters")
@@ -93,20 +93,19 @@ if __name__ == "__main__":
         help="Loading iteration for rendering",
     )
     parser.add_argument(
+        "--render_traj_path",
+        type=str,
+        choices=["ellipse", "interp"],
+        default="interp",
+        help="Rendering trajectory path to use",
+    )
+    parser.add_argument(
         "--render_mode",
         type=str,
         choices=["RGB", "Diffuse", "Specular"],
         default="RGB",
         help="Rendering mode to use",
     )
-    parser.add_argument(
-        "--render_traj_path",
-        type=str,
-        choices=["ellipse", "interp"],
-        default="ellipse",
-        help="Rendering trajectory path to use",
-    )
-    parser.add_argument("--image", action="store_true")
     args = parser.parse_args(sys.argv[1:])
 
     print("Loading model from " + args.model_path)
