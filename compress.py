a24ca9e015a66e59a289d4da6a1211262045c20a
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2025-05-05 16:47:55 -0700

    Update compress.py

--- compress.py
+++ compress.py
@@ -4,16 +4,11 @@ import sys
 from scene import BetaModel
 from argparse import ArgumentParser
 
-import time
-
 
 def compressing(args):
     beta_model = BetaModel()
     beta_model.load_ply(args.ply)
-    start_time = time.time()
     beta_model.save_png(os.path.dirname(args.ply))
-    end_time = time.time()
-    print(f"Compression time: {end_time - start_time:.2f} seconds")
 
 
 if __name__ == "__main__":
