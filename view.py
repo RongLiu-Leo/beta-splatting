9ee6a1e4dc31d978afde47b755b1730fc35c06d5
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2025-05-05 14:02:56 -0700

    Update view.py

--- view.py
+++ view.py
@@ -9,7 +9,12 @@ from scene.beta_viewer import BetaViewer
 
 def viewing(args):
     beta_model = BetaModel(args.sh_degree, args.sb_number)
-    beta_model.load_ply(args.ply)
+    if args.ply:
+        beta_model.load_ply(args.ply)
+    elif args.png:
+        beta_model.load_png(args.png)
+    else:
+        raise ValueError("You must provide either a .ply file or a .png folder")
     bg_color = [1, 1, 1] if args.white_background else [0, 0, 0]
     beta_model.background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
     server = viser.ViserServer(port=args.port, verbose=False)
@@ -36,6 +41,9 @@ if __name__ == "__main__":
     ModelParams(parser), ViewerParams(parser)
     parser.add_argument(
         "--ply", type=str, required=True, default=None, help="path to the .ply file"
+    )
+    parser.add_argument(
+        "--png", type=str, required=True, default=None, help="path to the png folder"
     )
 
     args = parser.parse_args()
